24fc6a7da0c9990b9b11f02701dc70e564122455
trblive <123860573+trblive@users.noreply.github.com>
2024-08-06 12:03:11 +0800

    init session 4 activities

--- session-04/assets/js/constructor_oop_examples/encapsulation.js
+++ session-04/assets/js/constructor_oop_examples/encapsulation.js
@@ -5,15 +5,15 @@ function Animal(name) {
   
     // Method encapsulated within the object
     this.sayName = function() {
-      console.log("My name is " + this.name);
+        console.log("My name is " + this.name);
     };
-  }
-  
-  // Creating instances using the constructor
-  var cat = new Animal("Fluffy");
-  var dog = new Animal("Buddy");
-  
-  // Abstraction: Details of the Animal implementation are abstracted away
-  cat.sayName(); // Outputs: "My name is Fluffy"
-  dog.sayName(); // Outputs: "My name is Buddy"
+}
+
+// Creating instances using the constructor
+var cat = new Animal("Fluffy");
+var dog = new Animal("Buddy");
+
+// Abstraction: Details of the Animal implementation are abstracted away
+cat.sayName(); // Outputs: "My name is Fluffy"
+dog.sayName(); // Outputs: "My name is Buddy"
   

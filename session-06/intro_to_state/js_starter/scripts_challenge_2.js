89232bc0854a5d60b52448bda58ec0e85203f189
trblive <123860573+trblive@users.noreply.github.com>
2024-08-20 13:51:18 +0800

    session 6 challenge activities

--- session-06/intro_to_state/js_starter/scripts_challenge_2.js
+++ session-06/intro_to_state/js_starter/scripts_challenge_2.js
@@ -5,13 +5,25 @@ let data = {
 };
 
 // The template
-
+let template = function (props) {
+	return '<ul>' +
+		props.books.map(function (book) {
+			return '<li>' + book + '</li>';
+		}).join('') + '</ul>';
+}
 
 // Render the template into the UI
-
-
+let app = document.querySelector('#app');
+app.innerHTML = template(data);
 
 // Update the UI
+data.books.push('Harry Potter IV');
+data.books.push('Harry Potter V');
+app.innerHTML = template(data);
 
+// manipulating the data
+data.books = data.books.slice(1);
+data.books.pop();
+data.books= data.books.filter((book) => book.length < 16);
 
-
+app.innerHTML = template(data);
